f8fae350306ab5f906af58adb2bad3041754c56a
Ruben Claassen <ruben.claassen@home.nl>
2023-02-07 12:05:57 +0100

    week2 YingYang

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -33,6 +33,24 @@ public class YingYang extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        GeneralPath path = new GeneralPath();
+
+        Area circle = new Area(new Ellipse2D.Double(100,100,350,350));
+        Area smallBlack = new Area(new Ellipse2D.Double(250, 175,50, 50));
+        Area smallWhite = new Area(new Ellipse2D.Double(250, 350,50, 50));
+        path.moveTo(275, 100);
+        path.curveTo(360, 110, 400,260,275, 275);
+        path.curveTo(150, 290, 190, 440, 275, 450);
+        path.curveTo(510, 450, 510, 100, 275,100);
+        graphics.setColor(Color.black);
+        graphics.draw(circle);
+        graphics.fill(path);
+        graphics.draw(path);
+        graphics.fill(smallBlack);
+        graphics.draw(smallBlack);
+        graphics.setColor(Color.white);
+        graphics.fill(smallWhite);
+        graphics.draw(smallWhite);
     }
 
 
