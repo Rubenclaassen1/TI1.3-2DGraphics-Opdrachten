f1a7eb19ed2d255e4da8fed2180a38dc5b0d0a40
Rubenclaassen1 <ruben.claassen@home.nl>
2023-02-27 14:27:51 +0100

    character can move and jump

--- Week3/002.MovingCharacter/src/MovingCharacter.java
+++ Week3/002.MovingCharacter/src/MovingCharacter.java
@@ -20,6 +20,16 @@ import org.jfree.fx.ResizableCanvas;
 
 public class MovingCharacter extends Application {
     private ResizableCanvas canvas;
+    private BufferedImage[] tiles;
+    private BufferedImage currentSprite;
+    int spriteIndex = 42;
+    int xPos = 0;
+    int yPos = 200;
+    double lastMoveTimer = 0;
+    int moveSpeed = 5;
+    int flipScaleX = 3;
+    boolean isJumping = false;
+    boolean isFlipped = false;
 
     @Override
     public void start(Stage stage) throws Exception
@@ -29,6 +39,9 @@ public class MovingCharacter extends Application {
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+
+        init();
+
         new AnimationTimer() {
             long last = -1;
 
@@ -47,19 +60,92 @@ public class MovingCharacter extends Application {
         stage.setTitle("Moving Character");
         stage.show();
         draw(g2d);
+
+        canvas.setOnMouseClicked(event -> {
+            isJumping = true;
+            spriteIndex = 17;
+
+        });
+    }
+
+    public void init(){
+        try {
+            BufferedImage image = ImageIO.read(getClass().getResource("images/sprite.png"));
+            tiles = new BufferedImage[65];
+
+            //knip de afbeelding op in 65 stukjes van 32x32 pixels.
+            for(int i = 0; i < 65; i++)
+                tiles[i] = image.getSubimage(64 * (i%8), 64 * (i/8), 64, 64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+        graphics.setBackground(Color.BLACK);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        AffineTransform character = new AffineTransform();
+        character.translate(xPos, yPos);
+        character.scale(flipScaleX, 3);
+
+        graphics.drawImage(currentSprite, character, null);
     }
 
 
     public void update(double deltaTime)
     {
+        lastMoveTimer += deltaTime;
+
+        if (spriteIndex == 50)
+        {
+            spriteIndex = 42;
+        }
+        currentSprite = tiles[spriteIndex];
+//        System.out.println(lastMoveTimer);
+        if (lastMoveTimer > 0.06)
+        {
+            spriteIndex++;
+            System.out.println(spriteIndex);
+            lastMoveTimer = 0;
+        }
+
+        if (xPos >= canvas.getWidth() - (currentSprite.getWidth() * flipScaleX))
+        {
+            isFlipped = true;
+        }
+
+        if (xPos < currentSprite.getWidth()){
+            isFlipped = false;
+        }
+
+        if (isFlipped)
+        {
+            xPos -= moveSpeed;
+            flipScaleX = -3;
+        }
+        else
+        {
+            xPos += moveSpeed;
+            flipScaleX = 3;
+        }
+
+
+        if (isJumping)
+        {
+            this.moveSpeed = 0;
+            if (spriteIndex == 25)
+            {
+                isJumping = false;
+                spriteIndex = 42;
+                this.moveSpeed = 5;
+            }
+        }
+
+
     }
 
     public static void main(String[] args)
